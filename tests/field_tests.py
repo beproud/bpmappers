a8a49e9a3f4ad6a2be1848fc71c3705f8d833e26
Shinya Okano <tokibito@gmail.com>
2012-08-08 12:37:57 +0900

    fieldsのテストコードを追加
    --HG--
    branch : v0.6dev-test-refactoring

--- tests/field_tests.py
+++ tests/field_tests.py
@@ -7,16 +7,13 @@ from bpmappers.exceptions import InvalidDelegateException
 class NonKeyFieldTest(TestCase):
     "Tests for NonKeyField class."
     def setUp(self):
-        self.callback = DummyCallback(returns="Spam")
+        self.callback = DummyCallback("Spam")
         self.field = fields.NonKeyField(self.callback)
 
     def test_get_value(self):
         value = self.field.get_value(None, None)
+        self.assertTrue(self.callback.called)
         self.assertEqual(value, self.callback.returns)
-
-    def test_as_value(self):
-        value = self.field.as_value(None, "Egg")
-        self.assertEqual(value, "Egg")
 
     def test_is_nonkey(self):
         self.assertEqual(self.field.is_nonkey, True)
@@ -99,13 +96,15 @@ class DelegateFieldTest(TestCase):
 class DelegateFieldBeforeFilterTest(TestCase):
     "DelegateField._before_filter"
     def setUp(self):
+        self.before_filter = DummyCallback({1: "Spam"})
         self.field = fields.DelegateField(
-            DummyMapper, before_filter=lambda v: {1: "Spam"})
+            DummyMapper, before_filter=self.before_filter)
 
     def test_get_value(self):
         value = self.field.get_value(
             DummyMapper(None), None)
-        self.assertEqual(value, {1: "Spam"})
+        self.assertEqual(value, self.before_filter.returns)
+        self.assertTrue(self.before_filter.called)
 
 
 class DelegateFieldRequiredTest(TestCase):
@@ -117,3 +116,91 @@ class DelegateFieldRequiredTest(TestCase):
     def test_none_value(self):
         value = self.field.get_value(DummyMapper(None), None)
         self.assertEqual(value, None)
+
+
+class ListDelegateFieldTest(TestCase):
+    "Test for ListDelegateField class."
+    def setUp(self):
+        self.field = fields.ListDelegateField(DummyMapper)
+
+    def test_get_value(self):
+        # TODO: need more
+        value = self.field.get_value(
+            DummyMapper(None), [{"Spam": "Egg"}, {"Bacon": "Egg"}])
+        self.assertEqual(value, [{"Spam": "Egg"}, {"Bacon": "Egg"}])
+
+    def test_is_nonkey(self):
+        self.assertEqual(self.field.is_nonkey, False)
+
+
+class ListDelegateFieldFilterTest(TestCase):
+    "ListDelegateField.filter"
+    def setUp(self):
+        self.filter = DummyCallback([1, 2, 3])
+        self.field = fields.ListDelegateField(DummyMapper, filter=self.filter)
+
+    def test_get_value(self):
+        value = self.field.get_value(
+            DummyMapper(None), [])
+        self.assertEqual(value, [1, 2, 3])
+        self.assertTrue(self.filter.called)
+
+
+class ListDelegateFieldAfterFilterTest(TestCase):
+    "ListDelegateField.after_filter"
+    def setUp(self):
+        self.after_filter = DummyCallback("Spam")
+        self.field = fields.ListDelegateField(
+            DummyMapper,
+            after_filter=self.after_filter)
+
+    def test_get_value(self):
+        value = self.field.get_value(
+            DummyMapper(None), [1, 2, 3])
+        self.assertEqual(value, ["Spam", "Spam", "Spam"])
+        self.assertTrue(self.after_filter.called)
+
+
+class NonKeyDelegateFieldTest(TestCase):
+    "Test for NonKeyDelegateField class."
+    def setUp(self):
+        self.callback = DummyCallback({"Spam": "Egg"})
+        self.field = fields.NonKeyDelegateField(
+            DummyMapper, callback=self.callback)
+
+    def test_get_value(self):
+        value = self.field.get_value(DummyMapper(None), None)
+        self.assertEqual(value, self.callback.returns)
+        self.assertTrue(self.callback.called)
+
+    def test_is_nonkey(self):
+        self.assertEqual(self.field.is_nonkey, True)
+
+
+class NonKeyListDelegateFieldTest(TestCase):
+    "Test for NonKeyListDelegateField class."
+    def setUp(self):
+        self.callback = DummyCallback({"Spam": "Egg"})
+        self.field = fields.NonKeyListDelegateField(
+            DummyMapper, callback=self.callback)
+
+    def test_get_value(self):
+        value = self.field.get_value(DummyMapper(None), None)
+        self.assertEqual(value, [self.callback.returns])
+        self.assertTrue(self.callback.called)
+
+    def test_is_nonkey(self):
+        self.assertEqual(self.field.is_nonkey, True)
+
+
+class NonKeyListDelegateFieldFilterTest(TestCase):
+    "NonKeyListDelegateField.filter"
+    def setUp(self):
+        self.filter = DummyCallback([1, 2, 3])
+        self.field = fields.NonKeyListDelegateField(
+            DummyMapper, filter=self.filter)
+
+    def test_get_value(self):
+        value = self.field.get_value(DummyMapper(None), [{"Spam": "Egg"}])
+        self.assertEqual(value, self.filter.returns)
+        self.assertTrue(self.filter.called)
--- tests/testing.py
+++ tests/testing.py
@@ -5,8 +5,10 @@ class DummyCallback(object):
     "Utility class for callback test."
     def __init__(self, returns=None):
         self.returns = returns
+        self.called = False
 
     def __call__(self, *args, **kwargs):
         self.args = args
         self.kwargs = kwargs
+        self.called = True
         return self.returns
