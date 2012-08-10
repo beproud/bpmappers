2c95cd78941540d98ace5413995b4e1224fde054
Shinya Okano <tokibito@gmail.com>
2012-08-10 12:43:59 +0900

    setup.py developコマンドで開発環境を用意できるように修正

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 import os
 from setuptools import setup, find_packages
 
+
 def read_file(filename):
-    filepath = os.path.join(os.path.dirname(os.path.dirname(__file__)), filename)
+    basepath = os.path.dirname(os.path.dirname(__file__))
+    filepath = os.path.join(basepath, filename)
     if os.path.exists(filepath):
         return open(filepath).read()
     else:
@@ -18,21 +20,25 @@ setup(
     author_email='shinya.okano@beproud.jp',
     url='http://tokibito.bitbucket.org/bpmappers/',
     classifiers=[
-      'Development Status :: 4 - Beta',
-      'Environment :: Plugins',
-      'Framework :: Django',
-      'Intended Audience :: Developers',
-      'License :: OSI Approved :: BSD License',
-      'Programming Language :: Python',
-      'Programming Language :: Python :: 2.5',
-      'Programming Language :: Python :: 2.6',
-      'Programming Language :: Python :: 2.7',
-      'Programming Language :: Python :: 3',
-      'Topic :: Software Development :: Libraries :: Python Modules',
+        'Development Status :: 4 - Beta',
+        'Environment :: Plugins',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     packages=find_packages(),
     keywords=['model', 'mapper', 'django'],
     license='BSD License',
     test_suite='nose.collector',
     tests_require=['nose'],
+    extras_require={
+        'django': ['Django'],
+        'test': ['nose', 'coverage', 'Django', 'unittest2'],
+    },
 )
